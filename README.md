f6c59e61d768a040cf14f80fb3b796fe5851e756
Alexander Zaytsev <zaytsev126@gmail.com>
2018-05-06 23:37:09 +0300

    fix path

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Drag'n'Drop
 
-[https://shoonia.github.io/dnd/](https://shoonia.github.io/dnd/)
+[https://shoonia.github.io/dnd](https://shoonia.github.io/dnd/index.html)
--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
+const ROOT = (process.env.NODE_ENV === 'production')
+  ? 'https://shoonia.github.io/dnd'
+  : 'http://localhost:3000';
+
 export const fetchAll = callback =>
-  fetch('/mock.json')
+  fetch(`${ROOT}/mock.json`)
     .then(response => response.json())
     .then(data => callback(null, data))
     .catch(error => callback(error));
